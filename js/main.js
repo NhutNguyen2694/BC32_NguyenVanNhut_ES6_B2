710b7bbd7393d94beb7e348c802350a71e62ba1a
NhutNguyen2694 <promise2694@gmail.com>
2022-09-08 00:01:04 +0700

    es

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let dataGlasses = [
+    { id: "G1", src: "./img/g1.jpg", virtualImg: "./img/v1.png", brand: "Armani Exchange", name: "Bamboo wood", color: "Brown", price: 150, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? " },
+    { id: "G2", src: "./img/g2.jpg", virtualImg: "./img/v2.png", brand: "Arnette", name: "American flag", color: "American flag", price: 150, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio." },
+    { id: "G3", src: "./img/g3.jpg", virtualImg: "./img/v3.png", brand: "Burberry", name: "Belt of Hippolyte", color: "Blue", price: 100, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
+    { id: "G4", src: "./img/g4.jpg", virtualImg: "./img/v4.png", brand: "Coarch", name: "Cretan Bull", color: "Red", price: 100, description: "In assumenda earum eaque doloremque, tempore distinctio." },
+    { id: "G5", src: "./img/g5.jpg", virtualImg: "./img/v5.png", brand: "D&G", name: "JOYRIDE", color: "Gold", price: 180, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?" },
+    { id: "G6", src: "./img/g6.jpg", virtualImg: "./img/v6.png", brand: "Polo", name: "NATTY ICE", color: "Blue, White", price: 120, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
+    { id: "G7", src: "./img/g7.jpg", virtualImg: "./img/v7.png", brand: "Ralph", name: "TORTOISE", color: "Black, Yellow", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam." },
+    { id: "G8", src: "./img/g8.jpg", virtualImg: "./img/v8.png", brand: "Polo", name: "NATTY ICE", color: "Red, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim." },
+    { id: "G9", src: "./img/g9.jpg", virtualImg: "./img/v9.png", brand: "Coarch", name: "MIDNIGHT VIXEN REMIX", color: "Blue, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet." }
+];
+
+
+dom("#vglassesList").innerHTML = dataGlasses.reduce((result,glass) =>{
+    return result +=`
+    <div class="col-4">
+        <img src="${glass.src}" width="100%" data-id="${glass.id}"></img>
+    </div>`
+},"")
+
+dom("#vglassesList").addEventListener("click",(evt) =>{
+    const eltype = evt.target.getAttribute("data-id")
+
+    // console.log("eltype",eltype)
+
+    function check(element) {
+        // console.log(element,dataGlasses)
+        return dataGlasses.find((value) => value.id === element)
+    }
+    
+    const checkglass = check(eltype)
+    
+    // console.log("checkglass",checkglass)
+    if(checkglass){
+        dom("#avatar").innerHTML = `
+        <div>
+            <img src="${checkglass.virtualImg}" width="100%"></img>
+        </div>
+        `
+        dom("#glassesInfo").innerHTML =`
+        <h5>${checkglass.name} - ${checkglass.brand}(${checkglass.color})</h5>
+        <span class="bg-danger p-1" >$${checkglass.price}</span>
+        <span style="color:green">Stocking</span>
+        <p class="mt-3">${checkglass.description}</p>
+        `
+        dom("#glassesInfo").style.display = "block"
+    }
+})
+
+
+
+//======================================
+function dom(selector){
+    return document.querySelector(selector)
+}
